ed91e29b04ff4db9b9878936af4061b0fb2d3c50
Seb <sebastian@c4k3.net>
2015-06-28 12:32:51 +0200

    Ensure that player deaths in events world does not delete portals

--- src/net/c4k3/Portals/PlayerDeath.java
+++ src/net/c4k3/Portals/PlayerDeath.java
@@ -1,5 +1,6 @@
 package net.c4k3.Portals;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -9,6 +10,13 @@ public class PlayerDeath implements Listener {
 
 	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled=true)
 	public void onPlayerDeathEvent(PlayerDeathEvent event) {
+
+		Player player = event.getEntity();
+		String world = player.getLocation().getWorld().getName();
+
+		if (!(world.equals("world") || world.equals("world_nether") || world.equals("world_the_end") || world.equals("pvp")))
+			return;
+
 		SQLite.delete_unset_portal(event.getEntity().getUniqueId());
 	}
 }
