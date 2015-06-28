b61d913196a44af97178bd0ab642a8b3c3a54d90
Seb <sebastian@c4k3.net>
2015-06-28 12:32:00 +0200

    Add PortalLocation class

---
+++ src/net/c4k3/Portals/PortalLocation.java
@@ -0,0 +1,19 @@
+package net.c4k3.Portals;
+
+import org.bukkit.block.Block;
+
+/**
+ * Basically just a pair pairing a Block that is the location of the portal,
+ * and the portal's ID.
+ *
+ */
+public class PortalLocation {
+	public Block block;
+	public int id;
+	
+	public PortalLocation(Block block, int id) {
+		this.block = block;
+		this.id = id;
+	}
+
+}
