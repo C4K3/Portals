589eedd4d8e056e49c5aba06a3bba6d3a0c6ca2e
Sebastian Blunt <sebastian@c4k3.net>
2016-02-11 21:05:34 +0100

    No more IDE

---
+++ compile.sh
@@ -0,0 +1,8 @@
+RT_JAR=/usr/lib/jvm/java-8-openjdk/jre/lib/rt.jar
+INCLUDES_DIR=../
+BUKKIT_TARGET=bukkit-1.8.8B1.jar
+DST_DIR=.
+
+javac -Xlint:all -bootclasspath "$RT_JAR:$INCLUDES_DIR/$BUKKIT_TARGET" -d ./ src/*
+jar -cfe $DST_DIR/Portals.jar net/simpvp/Portals/Portals ./*
+rm -rf net/
