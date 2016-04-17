34f9f74b622a2efb4439716020b77a20a2b45d4d
BuildTools <unconfigured@null.spigotmc.org>
2016-04-17 10:28:53 -0400

    Fix entity teleports

---
+++ src/PortalLocation.java
@@ -0,0 +1,20 @@
+package net.simpvp.Portals;
+
+import org.bukkit.block.Block;
+
+/**
+ * Basically just a pair pairing a Block that is the location of the portal,
+ * and the portal's ID.
+ *
+ */
+public class PortalLocation {
+	public Block block;
+	public int id;
+
+	public PortalLocation(Block block, int id) {
+		this.block = block;
+		this.id = id;
+	}
+
+}
+
